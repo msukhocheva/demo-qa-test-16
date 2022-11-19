077b63170bc48a4bd9e4266b40cf8ecd6d812aa4
msukhocheva <maryana.sukhocheva@btsdigital.kz>
2022-11-19 23:45:58 +0600

    add JUnit5Examples

---
+++ src/test/java/JUnit5Examples.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JUnit5Examples {
+    @BeforeAll
+    static void beforeAll(){
+        //open("google.com");
+        System.out.println("### beforeAll()");
+    }
+    @AfterAll
+    static void afterAll(){
+        //open("google.com");
+        System.out.println("### afterAll()");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        //open("google.com");
+        System.out.println("### beforeEach()");
+    }
+
+    @AfterEach
+    void afterEach(){
+
+        System.out.println("### afterEach()");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("### FirstTest()");
+        assertTrue(3>2);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("### secondTest()");
+       // assertFalse(3>2);
+    }
+}
